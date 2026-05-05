2dbb1f739406788ebbef046d9a097b87033f6537
iqbalmn-005 <iqbalnishad137@gmail.com>
2026-05-05 07:35:36 +0530

    Switch database configuration to PostgreSQL and update requirements

--- service1/settings.py
+++ service1/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'service1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'service',
+        'USER': 'postgres',
+        'PASSWORD': 'ikku05',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
